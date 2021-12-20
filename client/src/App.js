2820120387fedf7bcafbd70ad94d9521892ee86e
Yuval Tal <Yuval.Tal@Kornit.com>
2021-12-20 20:04:02 +0200

    comminicating with DB from fromtEnd

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [instName, setInstName] = useState("")
+  const [CFOName, setCFOName] = useState("")
+  const [amount, setAmount] = useState(0)
+
+  const addToList = ()=>{
+    // console.log(instName + CFOName + amount)
+    Axios.post("http://localhost:3001/insert", {
+      instName: instName,
+      CFOName: CFOName,
+      amount: amount,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD app with MERN</h1>
+
+      <label>Inst. Name:</label>
+      <input 
+        type="text"
+        onChange={(event)=>{
+          setInstName(event.target.value)
+        }}
+      />
+      <label>Inst. CFO:</label>
+      <input 
+        type="text"
+        onChange={(event)=>{
+          setCFOName(event.target.value)
+        }}
+      />
+      <label>Amount:</label>
+      <input 
+        type="number"
+        onChange={(event)=>{
+          setAmount(event.target.value)
+        }}
+      />
+      <button onClick={addToList}>Add to DB</button>
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require('cors')
 const app = express()
 
 const BudgetModel = require("./models/Budget")
 
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://yuvalt30:yt6073463@ezb.xoqka.mongodb.net/budget?retryWrites=true&w=majority", {
@@ -11,10 +13,28 @@ mongoose.connect("mongodb+srv://yuvalt30:yt6073463@ezb.xoqka.mongodb.net/budget?
 });
 
 
-app.get('/', async (req, res) => {
-    const budge = new BudgetModel({instName: "yeshiva", instCFO: "David", amount: 1000})
+app.get("/", async (req, res) => {
+    
+    const budge = new BudgetModel({instName: 'yesh', instCFO: 'dav', amount: 18})
 
     try{
+        await budge.save();
+        res.send("inserted data")
+    } catch(err){
+        console.log(err)
+    }
+})
+
+
+app.post("/insert", async (req, res) => {
+    const instName = req.body.instName
+    const CFOName = req.body.CFOName
+    const amount = req.body.amount
+ 
+    const budge = new BudgetModel({instName: instName, instCFO: CFOName, amount: amount});
+    try{
+        res.send(instName + CFOName + " amount = " + amount)
+
         await budge.save();
         res.send("inserted data")
     } catch(err){
