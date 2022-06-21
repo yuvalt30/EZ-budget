252b2f491372c9562a6bce2c0d61142ac41394f1
Yuval Tal <Yuval.Tal@Kornit.com>
2022-06-21 19:13:35 +0300

    added date range check in line chart

--- server/routes/tracks.js
+++ server/routes/tracks.js
@@ -31,8 +31,11 @@ router.get('/past', async (req, res) => {
         // trans[0] - newest, trans[trans.length-1] - oldest
         begin = req.query.begin ? new Date(req.query.begin) : trans[trans.length-1].date
         end = req.query.end ? new Date(req.query.end) : trans[0].date
-
-        past = new Array(helper.monthDiff(begin,end)+1).fill(0)
+        range = helper.monthDiff(begin,end)+1
+        if(range < 1) {
+            res.status(400).send(" Begin date must be earlier than End date")
+        }
+        past = new Array(range).fill(0)
         trans.forEach(tran => {
             if(tran.date >= begin && tran.date <= end)
                 past[helper.monthDiff(begin, tran.date)] += tran.amount
