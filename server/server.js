22eb19fb67b9b7ef44165b0b953731bdec0973f8
Yuval Tal <Yuval.Tal@Kornit.com>
2022-06-23 10:45:04 +0300

    app.use(express.urlencoded({limit: '50mb', extended: true}));

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express()
 var cors = require('cors')
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cors())
 
 const indexRouter = require('./routes/index')
