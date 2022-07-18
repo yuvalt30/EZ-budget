2238abb933e6e52faa7322573699e7d1fc2548a6
Yuval Tal <Yuval.Tal@Kornit.com>
2022-07-18 21:53:41 +0300

    plan from file

--- server/routes/plans.js
+++ server/routes/plans.js
@@ -13,19 +13,39 @@ router.get('/', async (req,res) => {
     res.send(await helper.getAllSecsBudgetAsync(req.query.year))
 })
 
-router.put('/:subId-:amount', async (req, res) => {
-    inserted = 0; er = 0;
+async function createNewBudgetAsync(subId, amount, year){
     try{
-        subId = req.params.subId
-        year = new Date().getFullYear()
-        let sub = await Sections.findById(subId)
+        // let sub = await Sections.findById(subId)
         update = await Plan.updateOne(
             {section: subId, year: year}
             ,
-            {amount: req.params.amount}
-            )
-        res.status(200).send(update.matchedCount ? 'updates:'+ update.modifiedCount : 'No such sub section')
-    } catch(e) {res.status(500).send(e)}
+            {amount: amount}
+        )
+    } catch(e) {return false}
+    return update.matchedCount ? true : false
+}
+
+router.put('/:subId-:amount', async (req, res) => {
+    if(await createNewBudgetAsync(req.params.subId, req.params.amount), new Date().getFullYear())
+    {
+        res.status(201).send()
+    } else {res.status(500).send('No such sub section')}
+})
+
+router.post('/file', async (req, res) => {
+    let inserted=0
+    let notExist=0
+    let e=0
+    await Promise.all(req.body.budgets.map(async budget => {
+        secId = await Sections.getSubIdByNames(budget.sectionName, budget.subSection)
+        if(secId == null) {notExist+=1}
+        else {if (await createNewBudgetAsync(secId, budget.amount/*, budget.year*/)) {inserted+=1}
+            else {e +=1}}
+    }))
+    console.log(inserted+" inserted, "+e+" errors, "+notExist+" non existing sections")
+    if(inserted){
+        res.status(201).send(e ? inserted+" inserted, "+e+" errors, ensure section and sub section exist" : "all "+inserted+" transactions inserted")
+    } else res.status(400).send("all "+e+" insertions failed, ensure section and sub section exist")
 })
 
 module.exports = router
